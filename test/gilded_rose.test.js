a110836b5d34c83f8e565d721f2dff1ef265b2ac
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-17 12:44:22 +0000

    Re-worded/re-factored first test for returning item name - now passing

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,16 @@
-const {Shop, Item} = require("../src/shop");
+const Shop = require("../src/shop");
+const Item = require("../src/item");
+const { describe } = require("yargs");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+describe("Gilded Rose", () => {
+
+  describe('updateQuality', () => {
+ 
+    it("can return item name when new item is passed to Shop", () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("+5 Dexterity Vest");
+    });
   });
+
 });
