accda08069ff2bd60bec8fe54a0d333162a3a404
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-17 15:48:13 +0000

    Quality cannot be less than 0, & Quality decreases by 2 when sellIn reaches 0 - tests passing

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -5,13 +5,13 @@ describe('Gilded Rose', () => {
 
   describe('normal item behaviour', () => {
     it('can return item name when new item is passed to Shop', () => {
-      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toBe("+5 Dexterity Vest");
     });
 
     it('can decrement a item`s sellIn rating after a day passes', () => {
-      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 0)]);
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(9)
     });
@@ -20,6 +20,18 @@ describe('Gilded Rose', () => {
       const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(9)
+    });
+
+    it('if an item`s sellIn value is < 0, quality decrements by 2', () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(8)
+    });
+
+    it('an item`s quality can never be less than 0', () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0)
     });
   });
 
