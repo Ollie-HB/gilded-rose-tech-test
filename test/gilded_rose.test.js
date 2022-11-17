cd9281c2b7e605c63087e1a6a5d3c2b717d28261
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-17 17:30:57 +0000

    Sulfurus test added - quality/sellIn value cannot be changed

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -10,7 +10,7 @@ describe('Gilded Rose', () => {
       expect(items[0].name).toBe("+5 Dexterity Vest");
     });
 
-    it('can decrement a item`s sellIn rating after a day passes', () => {
+    it('can decrement an item`s sellIn value after a day passes', () => {
       const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(9)
@@ -22,7 +22,7 @@ describe('Gilded Rose', () => {
       expect(items[0].quality).toEqual(9)
     });
 
-    it('if an item`s sellIn value is < 0, quality decrements by 2', () => {
+    it('if an item`s sellIn value is <= 0, quality decrements by 2', () => {
       const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 10)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toEqual(8)
@@ -43,11 +43,21 @@ describe('Gilded Rose', () => {
       expect(items[0].quality).toEqual(21)
     });
 
-    it('an item`s quality cannot be above 50', () => {
+    it('an item`s quality cannot be above 50 (excluding Sulfuras)', () => {
       const gildedRose = new Shop([new Item("Aged Brie", 20, 50)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toEqual(19)
       expect(items[0].quality).toEqual(50)
     });
   });
+
+  describe('Sulfuras behaviour', () => {
+    it('Sulfuras` quality and sellIn value do not change', () => {
+      const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 1, 80)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(1)
+      expect(items[0].quality).toEqual(80)
+    });
+  });
 });
+
