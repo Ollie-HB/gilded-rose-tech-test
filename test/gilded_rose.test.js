92309a02853c115db56a2007c2d82a06f27d8c88
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-17 15:34:37 +0000

    Basic 'normal' items behaviour tests passing

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,15 +1,25 @@
-const Shop = require("../src/shop");
-const Item = require("../src/item");
-const { describe } = require("yargs");
+const Shop = require('../src/shop');
+const Item = require('../src/item');
 
-describe("Gilded Rose", () => {
+describe('Gilded Rose', () => {
 
-  describe('updateQuality', () => {
- 
-    it("can return item name when new item is passed to Shop", () => {
+  describe('normal item behaviour', () => {
+    it('can return item name when new item is passed to Shop', () => {
       const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].name).toBe("+5 Dexterity Vest");
+    });
+
+    it('can decrement a item`s sellIn rating after a day passes', () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(9)
+    });
+
+    it('can decrement a item`s quality rating after a day passes', () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(9)
     });
   });
 
