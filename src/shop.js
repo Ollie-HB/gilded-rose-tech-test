bd6c997227ce4ad41696090a4d1c8c60a59cba30
Ollie Beck <ollieb94@hotmail.co.uk>
2022-11-18 13:09:27 +0000

    Added case statement (JS switch) so I can go through each item individually

--- src/shop.js
+++ src/shop.js
@@ -6,17 +6,25 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       this.updateSellIn(i)
-      this.updateNormalItemQuality(i)
-      this.minimumQuality(i)
+      this.itemBehaviours(i)
     }
     return this.items;
+  }
+
+  itemBehaviours(i) {
+    switch (this.items[i].name) {
+      default:
+        this.normalItemQuality(i);
+        break;
+    }
+    this.minimumQuality(i);
   }
   
   updateSellIn(i) {
     this.items[i].sellIn--;
   }
 
-  updateNormalItemQuality(i) {
+  normalItemQuality(i) {
      this.items[i].quality--;
     if (this.items[i].sellIn <= 0) {
       this.items[i].quality--;
